222a495869368218a2905949b4f9a274ece3665c
jenny-lemon <jenny@lemonclean.com.tw>
2026-03-30 02:28:28 +0800

    Remove sensitive fields from accounts data
    Removed email and password fields from account entries.

--- accounts.py
+++ accounts.py
@@ -1,31 +1,21 @@
 ACCOUNTS = {
     "台北": {
-        "email": "@lemonclean.com.tw",
-        "password": "",
         "folder": "01.台北專員",
         "address_keywords": ["台北市", "新北市"],
     },
     "台中": {
-        "email": "@lemonclean.com.tw",
-        "password": "",
         "folder": "02.台中專員",
         "address_keywords": ["台中市"],
     },
     "桃園": {
-        "email": "@lemonclean.com.tw",
-        "password": "",
         "folder": "03.桃園專員",
         "address_keywords": [],
     },
     "新竹": {
-        "email": "@lemonclean.com.tw",
-        "password": "",
         "folder": "04.新竹專員",
         "address_keywords": ["新竹"],
     },
     "高雄": {
-        "email": "@lemonclean.com.tw",
-        "password": "",
         "folder": "05.高雄專員",
         "address_keywords": ["高雄", "台南"],
     },
